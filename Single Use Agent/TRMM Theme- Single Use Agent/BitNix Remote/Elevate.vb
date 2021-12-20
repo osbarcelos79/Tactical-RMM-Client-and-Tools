36362866d0c16f142cc2629a2bbb6edbfdf83ee4
X1pe0 <9041120+X1pe0@users.noreply.github.com>
2021-12-20 09:00:28 -0500

    Add files via upload

---
+++ Single Use Agent/TRMM Theme- Single Use Agent/BitNix Remote/Elevate.vb
@@ -0,0 +1,32 @@
+﻿
+Imports System.Diagnostics
+Imports System.Security
+Imports System.IO
+Public Class Elevate
+
+
+    Private Sub Elevate_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim securePassword As New Security.SecureString()
+
+        For Each c As Char In TextBox3.Text
+
+            securePassword.AppendChar(c)
+        Next c
+        Dim proc As ProcessStartInfo = New ProcessStartInfo()
+        proc.FileName = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName
+        proc.UseShellExecute = False
+        proc.Domain = TextBox1.Text
+        proc.UserName = TextBox2.Text
+        proc.Password = securePassword
+        Try
+            Process.Start(proc)
+        Catch ex As Exception
+            MessageBox.Show("Invalid username or password.")
+        End Try
+    End Sub
+End Class
---
+++ Single Use Agent/TRMM Theme- Single Use Agent/BitNix Remote/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TRMM Remote")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("TRMM Remote")>
+<Assembly: AssemblyCopyright("")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("040f39fd-a093-41f4-b4c2-afe4953b330c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
